ba80d2c3acbe5cfe01b94257022678edc9ea58ce
Manuel Gutierrez <magutbar@gmail.com>
2023-03-24 14:02:32 +0100

    Final seccion 17

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -11,6 +11,11 @@ interface MenuItem {
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class SidemenuComponent {
@@ -45,5 +50,15 @@ export class SidemenuComponent {
     }
   ]
 
+  authMenu: MenuItem[] = [
+    {
+      texto: 'Registro',
+      ruta: './auth/registro'
+    },
+    {
+      texto: 'Login',
+      ruta: './auth/login'
+    }
+  ]
 
 }
--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,17 +17,36 @@ import { NgForm } from '@angular/forms';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
-  constructor() { }
+  nuevoJuego: string = '';
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Manuel',
+    favoritos: [
+      { id: 1, nombre: 'Destiny 2'},
+      { id: 2, nombre: 'Pokemon'}
+    ]
   }
 
   nombreValido(): boolean {
     return this.miFormulario?.controls.nombre?.invalid && this.miFormulario?.controls.nombre?.touched;
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
   }
 
   guardar() {
